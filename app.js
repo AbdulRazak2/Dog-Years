72d1540ba51bd54abdfa59904cb66c3499917018
abdulrazak2 <alhassan.razak@amalitech.org>
2021-09-12 14:52:43 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//his is my age;
+let myAge = 29;
+//my earlyyears 
+let earlyYears = 2;
+earlyYears *= 10.5;
+//subtract two from your age;
+laterYears = myAge - 2;
+//multiplication
+laterYears *= 4;
+//my years
+myAgeInDogYears = earlyYears + laterYears;
+//display my name
+let myName = "abdul";
+myName.toLowerCase();
+//display result
+console.log(`my name is ${myName}. Iam ${myAge}.years old in human years which is ${myAgeInDogYears} years old in dog years.`);
